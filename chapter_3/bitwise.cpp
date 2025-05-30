69acb2953fe6eaed4766378c6e1971086d5c1c72
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-05-31 00:02:04 +0900

    feat : chapter 3 비트단위 연산자

--- chapter_3/bitwise.cpp
+++ chapter_3/bitwise.cpp
@@ -4,6 +4,18 @@
 int main() {
 	using namespace std;
 
-	unsigned int a = 1;
-	cout << std::bitset<4>(a) << endl;
+	/*unsigned int a = 1024;
+	cout << std::bitset<16>(a) << endl;
+	cout << std::bitset<16>(~a) << endl;
+	cout << std::bitset<16>(a>>1) << endl;
+	cout << std::bitset<16>(a>>2) << endl;
+	cout << std::bitset<16>(a>>3) << endl;
+	cout << std::bitset<16>(a>>4) << endl;
+	cout << std::bitset<16>(a>>5) << endl;
+	cout << std::bitset<16>(a>>6) << endl;*/
+
+	cout << std::bitset<4>(0b0110 >> 2) << endl;
+	cout << std::bitset<4>(5 | 12) << endl;
+	cout << std::bitset<4>(5 ^ 12) << endl;
+	cout << std::bitset<4>(5 & 12) << endl;
 }
