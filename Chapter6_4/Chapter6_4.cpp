b2077b810653bd60820811a41b45b396e8ef19be
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-06-03 13:32:15 +0900

    Chapter6_4 : 배열과 선택,버블 정렬

--- Chapter6_4/Chapter6_4.cpp
+++ Chapter6_4/Chapter6_4.cpp
@@ -11,23 +11,36 @@ int main() {
 	const int length = 5;
 	int arr[length] = { 3,5,2,1,4 };
 
-	printArr(arr, length);
-	
-	int min_idx = 0;
-	for (int i = 0; i < length-1; i++) {
-		min_idx = i;
-		for (int idx = i + 1; idx < length; idx++) {
-			if (arr[min_idx] > arr[idx])
-				min_idx = idx;
-		}
-		int tmp = arr[min_idx];
-		arr[min_idx] = arr[i];
-		arr[i] = tmp;
+	//printArr(arr, length);
+	//
+	//for (int i = 0; i < length-1; i++) {
+	//	int min_idx = i;
+	//	for (int idx = i + 1; idx < length; idx++) {
+	//		if (arr[min_idx] > arr[idx])
+	//			min_idx = idx;
+	//	}
+	//	
+	//	//swap two values in the array
+	//	{
+	//		int tmp = arr[min_idx];
+	//		arr[min_idx] = arr[i];
+	//		arr[i] = tmp;
+	//	}
+	//	printArr(arr, length);
+	//}
 
+	//bubble sort
+	printArr(arr, length);
+	for (int i = length - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
 		printArr(arr, length);
 	}
-
-
 	cout << endl;
 	return 0;
 }
