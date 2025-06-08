076eb7b4add820b6c7b749eab795520a9921c258
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-06-09 00:23:39 +0900

    Chapter10_02 : aggregation으로 변경

--- Chapter10_02/Chapter10_02.cpp
+++ Chapter10_02/Chapter10_02.cpp
@@ -4,15 +4,22 @@
 int main() {
 	using namespace std;
 
+	Student std1("Jack Jack", 0);
+	Student std2("Dash", 1);
+	Student std3("Violet", 2);
+
+	Teacher t1("Prof. Hong");
+	Teacher t2("Prof. Good");
+
 	Lecture lec1("Introduction to Computer Programming");
-	lec1.assignTeacher(Teacher("Prof. Hong"));
-	lec1.registerStudent(Student("Jack Jack"));
-	lec1.registerStudent(Student("Dash", 1));
-	lec1.registerStudent(Student("Violet", 2));
+	lec1.assignTeacher(&t1);
+	lec1.registerStudent(&std1);
+	lec1.registerStudent(&std2);
+	lec1.registerStudent(&std3);
 
 	Lecture lec2("Computational Thinking");
-	lec2.assignTeacher(Teacher("Prof. Good"));
-	lec2.registerStudent(Student("Jack Jack"));
+	lec2.assignTeacher(&t2);
+	lec2.registerStudent(&std1);
 
 	//test
 	{
--- Chapter10_02/Lecture.h
+++ Chapter10_02/Lecture.h
@@ -8,8 +8,8 @@ class Lecture {
 private:
 	std::string m_name;
 
-	Teacher teacher;
-	std::vector<Student> students;
+	Teacher* teacher;
+	std::vector<Student*> students;
 
 public:
 	Lecture(const std::string&name_in)
@@ -21,27 +21,41 @@ public:
 		//do Not delete students
 	}
 
-	void assignTeacher(const Teacher& const teacher_in) {
+	/*void assignTeacher(const Teacher& const teacher_in) {
 		teacher = teacher_in;
+	}*/
+
+	void assignTeacher(Teacher* const teacher_input) {
+		teacher = teacher_input;
 	}
 
-	void registerStudent(const Student& const student_in) {
+	/*void registerStudent(const Student& const student_in) {
+		students.push_back(student_in);
+	}*/
+
+	void registerStudent(Student* const student_in) {
 		students.push_back(student_in);
 	}
 
-	void study() {
+	/*void study() {
 		std::cout << m_name << " Study " << std::endl << std::endl;
 
 		for (auto& element : students)
 			element.setIntel(element.getIntel() + 1);
+	}*/
+	void study() {
+		std::cout << m_name << " Study " << std::endl << std::endl;
+
+		for (auto& element : students)
+			element->setIntel(element->getIntel() + 1);
 	}
 
 	friend std::ostream& operator<<(std::ostream& os, const Lecture& lecture) {
 		os << lecture.m_name << std::endl;
-		os << lecture.teacher << std::endl;
+		os << *lecture.teacher << std::endl;
 
-		for (const auto& student : lecture.students)
-			os << student << std::endl;
+		for (const auto student : lecture.students)
+			os << *student << std::endl;
 		return os << std::endl;
 	}
 };
